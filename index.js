5d35399022b424dfe7d35d490f0a36d89a025678
shuvro baset <md.abdul.baset75@gmail.com>
2021-12-28 11:57:18 +0600

    config

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URI
+)
+.then(()=> console.log("Db connection successful"))
+.catch((err)=> console.log(err));
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Backend server is running!")
+});
