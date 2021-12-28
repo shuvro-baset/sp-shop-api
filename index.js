1263eaea7a8e9aee43db07e40be74cb13d055761
shuvro baset <md.abdul.baset75@gmail.com>
2021-12-28 12:17:49 +0600

    modeol

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+const userRoute = require('./routes/user')
+
 dotenv.config();
 
 mongoose.connect(
@@ -10,6 +12,11 @@ mongoose.connect(
 )
 .then(()=> console.log("Db connection successful"))
 .catch((err)=> console.log(err));
+
+app.use(express.json());
+app.use("/api/users", userRoute);
+
+
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running!")
