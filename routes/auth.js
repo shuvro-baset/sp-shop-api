52448905fa5b743078a29663e0f3ca43fce3971f
shuvro baset <md.abdul.baset75@gmail.com>
2021-12-28 15:21:13 +0600

    login register

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const User = require('../models/User')
-
+const CryptoJS = require("crypto-js");
 // register
 router.post("/register", async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString(),
     });
     try {
         const savedUser = await newUser.save();
@@ -18,5 +18,22 @@ router.post("/register", async (req, res) => {
     }
 });
 
+
+// Login user
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({username: req.body.username});
+        !user && res.status(401).json("wrong credentials!");
+        const hashedPassword = CryptoJS.AES.decrypt(user.password, process.env.PASS_SEC);
+        const password = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+        password !== req.body.password && res.status(401).json("wrong credentials!");
+    
+        res.status(200).json(user);
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router
